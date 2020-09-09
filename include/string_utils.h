eaf82d9586292f74d56811460b055570c9542045
Evtushenko Dmitry <evtushenko.2002@mail.ru>
2020-09-09 09:20:51 +0300

    documentation in porgress

--- include/string_utils.h
+++ include/string_utils.h
@@ -1,3 +1,6 @@
+/*!
+ * @file Header of some utilites for processing strings
+ */
 #ifndef STRING_UTILS_H
 #define STRING_UTILS_H
 
@@ -5,7 +8,23 @@
 #include <stdio.h>
 #include "bool.h"
 
+
+/*!
+ * @fn bool strless(const unsigned char* a, const unsigned char *b)
+ * @brief Compares strings lexicographicly
+ * Return true if a is lexicograficly less than b in read encodinc
+ * @param a first string, not NULL
+ * @param b second string, not NULL
+ *
+ * @return true if a < b in lexicographical terms
+ */
 bool strless(const unsigned char* a, const unsigned char *b);
+/*!
+ * @fn char* readstring(FILE *in)
+ * @brief Read string from file and return pointer to dynamic array
+ * @param in input file
+ * @return NULL file has ended, pointer no read string otherwise
+ */
 char* readstring(FILE *in);
 
 #endif
--- include/vector.h
+++ include/vector.h
@@ -1,9 +1,9 @@
 #ifndef VECTOR_H
 #define VECTOR_H
-
 /*!
  * @file Header of c++ vector alternative
  */
+
 
 #include <stddef.h>
 #include "bool.h"
@@ -35,7 +35,7 @@ struct Vector
 } typedef Vector;
 
 /*!
- * @fn Vector* create_vector(size_t len, size_t el_sz)
+ * @fn bool create_vector(Vector *v, size_t len, size_t el_sz)
  * @brief Creates new vector
  * Creates new vector with length of len of elements with size el_sz
  * Returns true if vector created or false if allocation failed
@@ -48,9 +48,41 @@ struct Vector
 
 bool create_vector(Vector* v, size_t len, size_t el_sz);
 
+/*!
+ * @fn bool push_back(Vector* v, void* elem)
+ * @brief Appends element to vector from behind
+ *
+ *
+ * @param v pointer to vector struct to write results
+ * @param elem pointer to the element to be pushed
+ *
+ * @return true if adding element succseed, false otherwise
+ */
+
 bool push_back(Vector* v, void* elem);
 
+/*!
+ * @fn void* get_element(Vector v, size_t pos)
+ * @brief Returns pointer to pos-element
+ *
+ *
+ * @param v pointer to vector struct to write results
+ * @param pos index of element
+ *
+ * @return pointer to pos-element
+ */
+
 void* get_element(Vector v, size_t pos);
+
+/*!
+ * @fn void clean_vector(Vector v)
+ * @brief Destroys vector
+ *
+ * Removes allocated array inside vector. Leaves initial value as a garbage
+ *
+ * @param v vector to clen
+ *
+ */
 
 void clean_vector(Vector v);
 
