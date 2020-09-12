e7ec1152dffe514a84b817fe0dcc241904a73ccc
Evtushenko Dmitry <evtushenko.2002@mail.ru>
2020-09-12 10:27:46 +0300

    Unicode done

--- src/string_utils.c
+++ src/string_utils.c
@@ -11,9 +11,10 @@ bool isin(char c, const char s[])
     return false;
 }
 
+const char IGN[] = " ,.-\t\\/\"\'!?:;()";
+
 bool strless(const unsigned char* a, const unsigned char *b)
 {
-    static const char IGN[] = " ,.-\t\\/\"\'!?:;";
     size_t i = 0;
     size_t j = 0;
     while (a[i] != '\0' && b[j] != '\0')
@@ -33,26 +34,41 @@ bool strless(const unsigned char* a, const unsigned char *b)
     return a[i] < b[j];
 }
 
+wchar_t get_utf8_rev(const unsigned char* a)
+{
+    static const unsigned char RUSSIAN[] = u8"абвгдеёжзийклмнопрстуфхцчшщыъьэюя"
+                                      "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЫЪЬЭЮЯ";
+    wchar_t c = (wchar_t)a[-1] * 256 + a[0];
+    for (int i = 0; i < sizeof(RUSSIAN) / sizeof(RUSSIAN[0]) / 2; ++i)
+    {
+        wchar_t d = (wchar_t)RUSSIAN[2 * i] * 256 + RUSSIAN[2 * i + 1];
+        if (c == d)
+            return c;
+    }
+    return a[0];
+}
+
 bool strless_reversed(const unsigned char* a, const unsigned char *b)
 {
-    static const char IGN[] = " ,.-\t\\/\"\'";
     size_t i = strlen(a);
     size_t j = strlen(b);
     while (i != 0 && j != 0)
     {
-        while (i != 0 && isin(a[i], IGN))
+        while (i != 0 && isin(a[i - 1], IGN))
             --i;
-        while (j != 0 && isin(b[j], IGN))
+        while (j != 0 && isin(b[j - 1], IGN))
             --j;
-        if (i != 0 && j != 0 && a[i] == b[j])
+        if (i != 0 && j != 0 && a[i - 1] == b[j - 1])
             --i, --j;
         else break;
     }
-    while (i != 0 && isin(a[i], IGN))
-        --i;
-    while (j != 0 && isin(b[j], IGN))
-        --j;
-    return a[i] < b[j];
+    wchar_t last_a = (i == 0 || isin(a[i - 1], IGN) ? '\0' : a[i - 1]);
+    if (i > 1)
+        last_a = get_utf8_rev(a + i - 1);
+    wchar_t last_b = (j == 0 || isin(b[j - 1], IGN) ? '\0' : b[j - 1]);
+    if (j > 1)
+        last_b = get_utf8_rev(b + j - 1);
+    return last_a < last_b;
 }
 
 char* readstring(FILE *in)
--- src/test_string_utils.c
+++ src/test_string_utils.c
@@ -1,5 +1,8 @@
 #include "vector.h"
 #include "string_utils.h"
+#include <stdlib.h>
+#include <string.h>
+#include <malloc.h>
 
 bool check_order(const unsigned char* a[], size_t s)
 {
@@ -9,24 +12,45 @@ bool check_order(const unsigned char* a[], size_t s)
     return true;
 }
 
-void test(const unsigned char *ordered_material[], size_t n)
+void test(const unsigned char *ordered_material[], size_t n,
+    bool (*cmp)(const unsigned char *a, const unsigned char *b))
 {
     for (int r = 0; r < n; ++r)
     {
         for (int l = 0; l < r; ++l)
         {
-            if (strless(ordered_material[r], ordered_material[l]))
+            if ((*cmp)(ordered_material[r], ordered_material[l]))
             {
-                printf("Wrong order: %d > %d\n", l, r);
+                printf("Wrong order at %d %d: %s > %s\n", l, r,
+                    ordered_material[l], ordered_material[r]);
+                exit(0);
                 return;
             }
         }
     }
 }
 
+void reverse(char s[])
+{
+    size_t l = strlen(s);
+    char t = '\0';
+    for (size_t i = 0; i < l / 2; ++i)
+    {
+        t = s[i];
+        s[i] = s[l - i - 1];
+        s[l - i - 1] = t;
+    }
+}
+
 int main(int argc, char const *argv[])
 {
-    const unsigned char* ordered_material[] =
+    char *exper[] =
+    {
+        "И где теперь ее сестра?",
+        "Не только грусть... душа моя,"
+    };
+    test(exper, sizeof(exper) / sizeof(exper[0]), &strless_reversed);
+    unsigned char *ordered_material[] =
     {
         "",
         ",ab.",
@@ -41,9 +65,94 @@ int main(int argc, char const *argv[])
         "abcde",
         "bcdeff",
         "bcf",
+        "А",
+        "Б",
+        "В",
+        "Г",
+        "Д",
+        "Е",
+        "Ж",
+        "З",
+        "И",
+        "Й",
+        "К",
+        "Л",
+        "М",
+        "Н",
+        "О",
+        "П",
+        "Р",
+        "С",
+        "Т",
+        "У",
+        "Ф",
+        "Х",
+        "Ц",
+        "Ч",
+        "Ш",
+        "Щ",
+        "Ъ",
+        "Ы",
+        "Ь",
+        "Э",
+        "Ю",
+        "Я",
+        "а",
+        "б",
+        "в",
+        "г",
+        "д",
+        "е",
+        "ж",
+        "з",
+        "и",
+        "й",
+        "к",
+        "л",
+        "м",
+        "н",
+        "о",
+        "п",
         "привет",
-        "тивирп"
+        "р",
+        "с",
+        "т",
+        "тивирпd",
+        "у",
+        "ф",
+        "х",
+        "ц",
+        "ч",
+        "ш",
+        "щ",
+        "ъ",
+        "ы",
+        "ь",
+        "э",
+        "ю",
+        "я",
     };
-    test(ordered_material, sizeof(ordered_material) / sizeof(ordered_material[0]));
+    test(ordered_material, sizeof(ordered_material) / sizeof(ordered_material[0]), &strless);
+
+    // Костыль, чтобы избежать ошибки сегментации при попытки изменить литерал
+    // (спасибо, read-only .text)
+    unsigned char *reversed_material[sizeof(ordered_material) / sizeof(ordered_material[0])] = {};
+    for (int i = 0; i < sizeof(ordered_material)/ sizeof(ordered_material[0]); ++i)
+    {
+        reversed_material[i] = calloc(strlen(ordered_material[i]) + 1, 1);
+        strcpy(reversed_material[i], ordered_material[i]);
+        printf("%s\n", reversed_material[i]);
+        for (int j = 0; j < strlen(reversed_material[i]); ++j)
+            printf("%2x ", reversed_material[i][j]);
+        putchar('\n');
+        reverse(reversed_material[i]);
+        printf("%s\n", reversed_material[i]);
+        for (int j = 0; j < strlen(reversed_material[i]); ++j)
+            printf("%2x ", reversed_material[i][j]);
+        putchar('\n');
+    }
+    test(reversed_material, sizeof(ordered_material) / sizeof(ordered_material[0]), &strless_reversed);
+
+
     return 0;
 }
